a6e4ea986cbfc2ba1aef4734982c581e2c03db5b
AMR BY <bouyoussefamr@gmail.com>
2023-08-20 22:06:28 +0000

    mandatory

--- _printf.c
+++ _printf.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include <stdarg.h>
+#include <unistd.h>
 /**
  * _printf - prints anything
  * @format: list of types per arguments passed
@@ -10,33 +12,32 @@ int _printf(const char *format, ...)
 	int i = 0, j = 0, k = 0;
 	char *arg;
 	va_list p;
-	void (*fun)(va_list, int);
 
 	if (format == NULL)
 		return (-1);
 
-		va_start(p, format);
-		while (format[i] != '\0')
+	va_start(p, format);
+	while (format[i] != '\0')
+	{
+		if (format[i] != '%')
 		{
-			if (format[i] != '%')
-			{
-				write(1, &format[i], 1);
-				k++;
-				i++;
-			}
-			else
-			{
-				arg = va_arg(p, char*);
-				while (arg && arg[j])
+			write(1, &format[i], 1);
+			k++;
+			i++;
+		}
+		else
+		{
+			arg = va_arg(p, char*);
+			while (arg && arg[j])
 				{
 					write(1, &arg[j], j);
 					j++;
 					k++;
 				}
-				va_end(p);
-				i = i + 2;
-			}
+			va_end(p);
+			i = i + 2;
 		}
+	}
 
 	return (k);
 }
